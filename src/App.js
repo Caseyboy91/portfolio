5b59b9e277078e45715fb30b94f9726159466531
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-02-22 11:45:27 -0800

    create header navbar, create pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import About from "./Pages/About/About.js";
+import Portfolio from "./Pages/Portfolio/Portfolio.js";
+import Contact from "./Pages/Contact/Contact.js";
+
 import "./App.css";
 
 function App() {
+  document.title = "Casey Davis | Software Engineer";
   return (
     <>
-      <div className="container ">
-        <h1>Hello, World!</h1>
-      </div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
