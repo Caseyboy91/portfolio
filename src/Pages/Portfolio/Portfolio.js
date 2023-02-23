203d319ba04b5faa1f62776bc7e462d3984e85be
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-02-23 12:36:41 -0800

    add portfolio content for runnit and ham it up

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,6 +1,8 @@
+import GitHub from "../../assets/icons/github.svg";
+
 const Portfolio = () => {
   return (
-    <section className="container">
+    <section className="container py-3">
       <div className="row">
         <div className="col">
           <div>
@@ -8,6 +10,196 @@ const Portfolio = () => {
             <hr />
           </div>
         </div>
+      </div>
+      {/* start of carousel */}
+      <div class="row pt-5 align-items-center">
+        <div className="col-md-4 pb-5">
+          <div>
+            <h2>Runnit</h2>
+          </div>
+
+          <p className="lead fs-5">
+            Similar to services like DoorDash, Uber Eats, and Postmates, Runnit
+            will deliver drinks, snacks, and merch directly to your seat so you
+            don't have to leave your favorite show, concert, or event.
+          </p>
+          <p className="lead fs-6">
+            This app was built as my Capstone project at BrainStation using
+            React, Axios, Express, Node, Bootstrap, Sass, and Stripe.
+          </p>
+          <p className="fs-6 d-flex justify-content-center lead text-secondary">
+            - Watch the demo video on slide 3 -
+          </p>
+          <div className="col d-flex justify-content-center ">
+            <a
+              className="text-decoration-none text-reset img-fluid me-3"
+              href="https://github.com/Caseyboy91/runnit"
+              target={"_blank"}
+            >
+              <p className="mb-0">Client</p>
+              <img className="ms-2" src={GitHub} alt="Client" />
+            </a>
+            <a
+              className="text-decoration-none text-reset img-fluid me-3"
+              href="https://github.com/Caseyboy91/runnit-api"
+              target={"_blank"}
+            >
+              <p className="mb-0">Server</p>
+              <img className="ms-2" src={GitHub} alt="Server" />
+            </a>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div
+            id="slides-with-controls"
+            class="carousel slide carousel-fade"
+            data-bs-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  class="d-block w-100"
+                  src="https://i.imgur.com/rhrVMuZ.png"
+                  alt="runnit"
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  class="d-block w-100"
+                  src="https://i.imgur.com/P6wzt3J.png"
+                  alt="runnit screenshots"
+                />
+              </div>
+              <div class="carousel-item ">
+                <a
+                  href="https://www.loom.com/share/ba27565872f64797a2e69f2a3077779b"
+                  target={"_blank"}
+                  rel={"noreferrer"}
+                >
+                  <img
+                    class="d-block w-100"
+                    src="https://i.imgur.com/ZptmT6g.png"
+                    alt="runnit video link"
+                  />
+                </a>
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#slides-with-controls"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#slides-with-controls"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <hr />
+      </div>
+
+      {/* 2nd */}
+      <div class="row py-5 align-items-center d-flex flex-row-reverse ">
+        <div className="col-md-4 pb-5">
+          <div>
+            <h2>Ham It Up</h2>
+          </div>
+
+          <p className="lead">
+            Having your favorite Broadway Show at your fingertips has never been
+            easier than with Ham It Up.
+          </p>
+
+          <p className="lead fs-6">
+            This app was built during a hackathon at BrainStation as an exercise
+            in paired-programming. We designed a prototype using React, Sass,
+            Axios, and JSON.
+          </p>
+          <p className="fs-6 d-flex justify-content-center lead text-secondary">
+            - Check out the code -
+          </p>
+          <div className="col d-flex justify-content-center ">
+            <a
+              className="text-decoration-none text-reset img-fluid me-3"
+              href="https://github.com/Caseyboy91/ham-it-up"
+              target={"_blank"}
+            >
+              <p className="mb-0">Client</p>
+              <img className="ms-2" src={GitHub} alt="Client" />
+            </a>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div
+            id="slides-with-controls2"
+            class="carousel slide carousel-fade"
+            data-bs-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  class="d-block w-100"
+                  src="https://i.imgur.com/NLo5wLe.png"
+                  alt="ham it up"
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  class="d-block w-100"
+                  src="https://i.imgur.com/SRXsQTj.png"
+                  alt="ham it up cards"
+                />
+              </div>
+              <div class="carousel-item ">
+                <img
+                  class="d-block w-100"
+                  src="https://i.imgur.com/qqpInNR.png"
+                  alt="card info"
+                />
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#slides-with-controls2"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#slides-with-controls2"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <hr />
       </div>
     </section>
   );
