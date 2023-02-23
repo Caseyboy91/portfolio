6184e003fdb8a30ac03a984369f04e7d63f5069e
Casey Davis <111889731+Caseyboy91@users.noreply.github.com>
2023-02-23 13:09:07 -0800

    initial commit

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,8 +1,15 @@
 const Contact = () => {
   return (
-    <>
-      <h1>Contact</h1>
-    </>
+    <section className="container py3">
+      <div className="row">
+        <div className="col">
+          <div>
+            <h1>Contact</h1>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
